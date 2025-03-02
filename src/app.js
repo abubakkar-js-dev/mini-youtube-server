4bcd23b4539e0db79e30fc20e8ce0064d63e38e2
abubakkar-js-dev <mdabubakkars182@gmail.com>
2025-03-02 07:20:16 +0600

    utils setup done

--- src/app.js
+++ src/app.js
@@ -1,6 +1,21 @@
 import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 const app = express();
+
+
+// middlewares
+app.use(express.json({limit: '16kb'}));
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true
+})); // to allow requests from the client
+
+app.use(express.urlencoded({extended: true, limit: '16kb'})); // to parse form data
+app.use(express.static('public')); // to serve static files 
+
+app.use(cookieParser()); // to parse cookies
 
 
 
