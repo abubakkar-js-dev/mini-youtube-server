382d6a8a45d01545424b641b452648c23dd4a49d
abubakkar-js-dev <mdabubakkars182@gmail.com>
2025-04-08 08:25:35 +0600

    updated multer configuration

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,8 +1,24 @@
 import multer from 'multer';
+import path from 'path';
+import {fileURLToPath} from 'url'
+import fs from 'fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// navigate up from middleware to the project root
+const projectRoot = path.resolve(__dirname,'../../');
+const tempUploadPath = path.join(projectRoot,'public/temp');
+
+// Ensure the directory exists
+if(!fs.existsSync(tempUploadPath)){
+    fs.mkdirSync(tempUploadPath,{recursive:true});
+    console.log(`Temp upload directory created at: ${tempUploadPath}`);
+}
 
 const storage = multer.diskStorage({
     destination: function(req,file,cb){
-        cb(null,'/public/temp')
+        cb(null,path.join(__dirname,'/public/temp'))
     },
     filename: function(req,file,cb){
         // const uniqueSuffix = Date.now() + '-' + Math.round(Math.random()*1E9)
